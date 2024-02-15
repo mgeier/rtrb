05d34f141dad1ccfb84c08fa9151e673ba820459
Matthias Geier <Matthias.Geier@gmail.com>
2024-02-15 19:17:33 +0100

    Implement core Error instead of std

--- src/lib.rs
+++ src/lib.rs
@@ -741,8 +741,7 @@ pub enum PopError {
     Empty,
 }
 
-#[cfg(feature = "std")]
-impl std::error::Error for PopError {}
+impl core::error::Error for PopError {}
 
 impl fmt::Display for PopError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -759,8 +758,7 @@ pub enum PeekError {
     Empty,
 }
 
-#[cfg(feature = "std")]
-impl std::error::Error for PeekError {}
+impl core::error::Error for PeekError {}
 
 impl fmt::Display for PeekError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -777,8 +775,7 @@ pub enum PushError<T> {
     Full(T),
 }
 
-#[cfg(feature = "std")]
-impl<T> std::error::Error for PushError<T> {}
+impl<T> core::error::Error for PushError<T> {}
 
 impl<T> fmt::Debug for PushError<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
